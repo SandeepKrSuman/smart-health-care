45231a35b4fdbf3e90ab961a6b30f84674ca8841
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2022-11-16 21:13:12 +0530

    Health Evaluation

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import DashBar from "../DashBar/DashBar";
+import HealthForm from "../HealthForm/HealthForm";
 
 import "./DashBoard.css";
 
@@ -7,8 +8,7 @@ export default function DashBoard() {
   return (
     <Fragment>
       <DashBar />
-      <h1>My Dashboard!</h1>
-      <p>welcome</p>
+      <HealthForm />
     </Fragment>
   );
 }
