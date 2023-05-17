099566c4876a6c8d3726b5dcf4f4b9dd4753eaa1
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-05-17 23:34:45 +0530

    precise threshold calculation using variance and std deviation

--- src/components/Threshold/threshold.js
+++ src/components/Threshold/threshold.js
@@ -4,6 +4,7 @@ import heartData from "./heart.csv";
 const age = sessionStorage.getItem("age");
 const gender = sessionStorage.getItem("gender");
 
+// Calculation of heart rate threshold
 const calcHeartThreshold = (data) => {
   const filteredData = data.filter(
     (row) => row.Age === age && row.Sex === gender
@@ -11,27 +12,51 @@ const calcHeartThreshold = (data) => {
 
   if (filteredData.length === 0) return 120;
 
+  const count = filteredData.length;
   const sum = filteredData.reduce((acc, row) => acc + parseInt(row.MaxHR), 0);
-  const mean = sum / filteredData.length;
+  const mean = sum / count;
 
-  return Math.round(mean);
+  const variance =
+    filteredData.reduce((acc, row) => {
+      const hr = parseInt(row.MaxHR);
+      return acc + (hr - mean) ** 2;
+    }, 0) / count;
+
+  const stdDeviation = Math.sqrt(variance);
+  const factor = 1;
+  const threshold = mean + factor * stdDeviation;
+
+  return Math.round(threshold);
 };
 
+// Calculation of sugar level threshold
 const calcSugarThreshold = (data) => {
-  //   const filteredData = data.filter(
-  //     (row) => row.Age === age && row.Sex === gender
-  //   );
-  //   const sum = filteredData.reduce(
-  //     (acc, row) => acc + parseInt(row.Cholesterol),
-  //     0
-  //   );
-  //   const mean = sum / filteredData.length;
+  // const filteredData = data.filter(
+  //   (row) => row.Age === age && row.Sex === gender
+  // );
 
-  //   return Math.round(mean);
+  // if (filteredData.length === 0) return 126;
+
+  // const count = filteredData.length;
+  // const sum = filteredData.reduce((acc, row) => acc + parseInt(row.Cholesterol), 0);
+  // const mean = sum / count;
+
+  // const variance =
+  //   filteredData.reduce((acc, row) => {
+  //     const sug = parseInt(row.Cholesterol);
+  //     return acc + (sug - mean) ** 2;
+  //   }, 0) / count;
+
+  // const stdDeviation = Math.sqrt(variance);
+  // const factor = 1;
+  // const threshold = mean + factor * stdDeviation;
+
+  // return Math.round(threshold);
 
   return 126;
 };
 
+// calculation of pressure threshold
 const calcPressureThreshold = (data) => {
   const filteredData = data.filter(
     (row) => row.Age === age && row.Sex === gender
@@ -39,13 +64,24 @@ const calcPressureThreshold = (data) => {
 
   if (filteredData.length === 0) return 100;
 
+  const count = filteredData.length;
   const sum = filteredData.reduce(
     (acc, row) => acc + parseInt(row.RestingBP),
     0
   );
-  const mean = sum / filteredData.length;
+  const mean = sum / count;
 
-  return Math.round(mean);
+  const variance =
+    filteredData.reduce((acc, row) => {
+      const bp = parseInt(row.RestingBP);
+      return acc + (bp - mean) ** 2;
+    }, 0) / count;
+
+  const stdDeviation = Math.sqrt(variance);
+  const factor = 1;
+  const threshold = mean + factor * stdDeviation;
+
+  return Math.round(threshold);
 };
 
 const findThreshold = () => {
